96a346ae1bebc8a5b8b808a40c19d613e1214e10
Eduardo <140274812+eduferrei@users.noreply.github.com>
2023-09-28 10:56:23 -0300

    atualizando o scripr para botão novo

--- script.js
+++ script.js
@@ -1,59 +1,30 @@
 "use strict";
 
 const sons = {
-  A: {
-    audio: 'sounds/cartoon-jump-6462.mp3',
-    count: 0 // Inicializa o contador com zero
-  },
-  //A: {audio:`sounds/cartoon-jump-6462.mp3`, count:0},
-  S: {audio:`sounds/failure-drum-sound-effect-2-7184.mp3`, count:0},
-  D: {audio:`sounds/funny-cartoon-cry-144756.mp3`, count:0},
-  F: {audio:`sounds/funny-reaction-120318.mp3`, count:0},
-  G: {audio:`sounds/funny-sound-effect-for-quotjack-in-the-boxquot-sound-ver1-110923.mp3`, count:0},
-  H: {audio:`sounds/man-scream-121085.mp3`, count:0},
-  J: {audio:`sounds/my-good-120319.mp3`, count:0},
-  K: {audio:`sounds/pipe-117724.mp3`, count:0},
-  L: {audio:`sounds/very-infectious-laughter-117727.mp3`, count:0},
-};
+  A: `sounds/cartoon-jump-6462.mp3`,
+  S: `sounds/failure-drum-sound-effect-2-7184.mp3`,
+  D: `sounds/funny-cartoon-cry-144756.mp3`,
+  F: `sounds/funny-reaction-120318.mp3`,
+  G: `sounds/funny-sound-effect-for-quotjack-in-the-boxquot-sound-ver1-110923.mp3`,
+  H: `sounds/man-scream-121085.mp3`,
+  J: `sounds/my-good-120319.mp3`,
+  K: `sounds/pipe-117724.mp3`,
+  L: `sounds/very-infectious-laughter-117727.mp3`,
+}
 
-const criarDiv = (letra) => {
-  const divContainer = document.createElement("div");
-  divContainer.classList.add("key-container");
-
-  const divBotao = document.createElement("div");
-  divBotao.classList.add("key");
-  divBotao.textContent = letra;
-  divBotao.id = letra;
-
-  const contadorDiv = document.createElement('div');
-  contadorDiv.classList.add('contador');
-  contadorDiv.id = `contador-${letra}`;
-  contadorDiv.textContent = `Contagem: 0`; // Inicializa o contador em zero
-
-  divContainer.appendChild(divBotao);
-  divContainer.appendChild(contadorDiv);
-
-  document.getElementById("container").appendChild(divContainer);
-};
-
-const exibir = (sons) => {
-  Object.keys(sons).forEach((letra) => {
-    criarDiv(letra);
-    criarContador(letra);
-  });
+const criarDiv = (texto) => {
+  const div = document.createElement("div");
+  div.classList.add("key");
+  div.textContent = texto;
+  div.id = texto;
+  document.getElementById("container").appendChild(div);
 };
 
 
-const criarContador = (letra) => {
-  const contadorDiv = document.createElement('div');
-  contadorDiv.classList.add('contador');
-  contadorDiv.id = `contador-${letra}`;
-  contadorDiv.textContent = `Contagem: 0`; // Inicializa o contador em zero
-  document.getElementById('contador').appendChild(contadorDiv);
-};
+const exibir = (sons) => Object.keys(sons).forEach(criarDiv);
 
 const tocarSom = (letra) => {
-  const audio = new Audio(sons[letra].audio);
+  const audio = new Audio(sons[letra]);
   audio.play();
 };
 
@@ -66,31 +37,29 @@ const removerEfeito = (letra) => {
 };
 
 const ativarDiv = (evento) => {
-  const letra = evento.type == 'click' ? evento.target.id : evento.key.toUpperCase();
+  const letra = evento.type == "click" ? evento.target.id : evento.key.toUpperCase();
 
   const letraPressionada = sons.hasOwnProperty(letra);
   if (letraPressionada) {
     adicionarEfeito(letra);
     tocarSom(letra);
     removerEfeito(letra);
-
-    // Atualiza o contador da tecla
-    sons[letra].count++;
-
-    // Atualiza a exibição do contador
-    atualizarContador(letra);
-  }
-};
-
-const atualizarContador = (letra) => {
-  const contadorElement = document.getElementById(`contador-${letra}`);
-  if (contadorElement) {
-    contadorElement.textContent = `Contagem: ${sons[letra].count}`;
   }
 };
 
 exibir(sons);
 document.getElementById("container").addEventListener("click", ativarDiv);
 document.addEventListener("keydown", ativarDiv);
+
+const randomSoundButton = document.getElementById("randomSoundButton");
+
+// Função para reproduzir um som aleatório
+const reproduzirSomAleatorio = () => {
+  const letras = Object.keys(sons);
+  const letraAleatoria = letras[Math.floor(Math.random() * letras.length)];
+  tocarSom(letraAleatoria);
+};
+
+randomSoundButton.addEventListener("click", reproduzirSomAleatorio);
 
 // window.addE...;
